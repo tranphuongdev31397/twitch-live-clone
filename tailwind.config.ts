d57d61836101c9e044439adc35ccbb60cf093d2f
Eddie Tran <tranphuongdev31397@gmail.com>
2024-01-21 13:05:56 +0700

    update: update config tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,6 +4,7 @@ const config = {
   darkMode: ["class"],
   content: [
     "./pages/**/*.{ts,tsx}",
+    "./layouts/**/*.{ts,tsx}",
     "./components/**/*.{ts,tsx}",
     "./app/**/*.{ts,tsx}",
     "./src/**/*.{ts,tsx}",
